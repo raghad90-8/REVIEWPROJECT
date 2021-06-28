575a759f0b6d9f65ae7a15f7cf59c6feff34c5e4
رغد العنزي <app02977@gmail.com>
2021-06-29 00:30:30 +0300

    Add project files.

---
+++ SOCIAL.DOMAIN/TYPES/ENTITY.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SOCIAL.DOMAIN.TYPES
+{
+    public abstract class ENTITY<TId> : IEquatable<ENTITY<TId>>
+    {
+        #region Constructors and properties
+        protected ENTITY(TId id)
+        {
+            if (!IsValidId(id))
+                throw new ArgumentException();
+
+            Id = Id;
+        }
+        #endregion
+
+        public TId Id { get; private set; }
+
+        #region IEquatable implementation
+        public bool Equals(ENTITY<TId> other)
+        {
+            return Id.GetHashCode() == other.Id.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as ENTITY<TId>);
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+
+        public static bool operator ==(ENTITY<TId> lhs, ENTITY<TId> rhs)
+        {
+            return lhs.Equals(rhs);
+        }
+
+        public static bool operator !=(ENTITY<TId> lhs, ENTITY<TId> rhs)
+        {
+            return !(lhs == rhs);
+        }
+        #endregion
+
+        #region Private methods
+        private bool IsValidId(TId id)
+        {
+            return id is int || id is Guid;
+        }
+        #endregion
+
+    }
+}
+
---
+++ SOCIAL.DOMAIN/TYPES/VALUEOBJECTS.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace SOCIAL.DOMAIN.TYPES
+{
+    public abstract class VALUEOBJECTS : IEquatable<VALUEOBJECTS>
+    {
+
+        protected abstract IEnumerable<object> GetAtomicValues();
+
+        #region IEquatable implementation
+        public bool Equals(VALUEOBJECTS other)
+        {
+            if (other == null || other.GetType() != GetType())
+                return false;
+
+            IEnumerator<object> thisValues = GetAtomicValues().GetEnumerator();
+            IEnumerator<object> otherValues = other.GetAtomicValues().GetEnumerator();
+
+            while (thisValues.MoveNext() && otherValues.MoveNext())
+            {
+                if (ReferenceEquals(thisValues.Current, null) ^ ReferenceEquals(otherValues.Current, null))
+                    return false;
+
+                if (thisValues.Current != null && !thisValues.Current.Equals(otherValues.Current))
+                    return false;
+            }
+
+            return !thisValues.MoveNext() && otherValues.MoveNext();
+
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(this, obj))
+                return true;
+
+            return Equals(obj as VALUEOBJECTS);
+        }
+
+        public override int GetHashCode()
+        {
+            return GetAtomicValues()
+                .Select(x => x != null ? x.GetHashCode() : 0)
+                .Aggregate((x, y) => x ^ y);
+        }
+
+        public static bool operator ==(VALUEOBJECTS left, VALUEOBJECTS right)
+        {
+            if (ReferenceEquals(left, null) ^ ReferenceEquals(right, null))
+                return false;
+
+            return ReferenceEquals(left, null) || left.Equals(right);
+        }
+
+        public static bool operator !=(VALUEOBJECTS left, VALUEOBJECTS right)
+        {
+            return !(left == right);
+        }
+        #endregion
+    }
+}
